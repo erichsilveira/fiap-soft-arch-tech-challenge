0e531a9d7264b64f8e3e9c064c5b434d0569b8c8
erichsilveira <erichnp@me.com>
2023-10-12 17:17:42 -0300

    feat(existsCustomer): added HEAD customers endpoint to verify customer existence

--- src/main/java/com/fiap/techchallenge/adapter/in/rest/CustomerController.java
+++ src/main/java/com/fiap/techchallenge/adapter/in/rest/CustomerController.java
@@ -3,14 +3,18 @@ package com.fiap.techchallenge.adapter.in.rest;
 import com.fiap.techchallenge.adapter.in.rest.data.request.CustomerRegistrationRequest;
 import com.fiap.techchallenge.adapter.in.rest.mapper.CustomerRestMapper;
 import com.fiap.techchallenge.application.domain.model.Customer;
+import com.fiap.techchallenge.application.port.in.customer.ExistsCustomerUseCase;
 import com.fiap.techchallenge.application.port.in.customer.RegisterCustomerUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final RegisterCustomerUseCase registerUseCase;
+    private final ExistsCustomerUseCase existsCustomerUseCase;
 
     private final CustomerRestMapper restMapper;
 
@@ -30,4 +35,11 @@ public class CustomerController {
 
         return new ResponseEntity<>(domainEntity, HttpStatus.CREATED);
     }
+
+    @RequestMapping(method = {RequestMethod.HEAD})
+    ResponseEntity<String> exists(@RequestParam @Valid String cpf) {
+        boolean exists = existsCustomerUseCase.existsCustomer(cpf);
+        return new ResponseEntity<>(exists ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+    }
+
 }
