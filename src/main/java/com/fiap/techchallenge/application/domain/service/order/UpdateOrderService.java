f0fc8f68ec07e9cfc0885ee8155615323f368a89
erichsilveira <erichnp@me.com>
2023-10-28 18:47:40 -0300

    feat: added logs and log config

--- src/main/java/com/fiap/techchallenge/application/domain/service/order/UpdateOrderService.java
+++ src/main/java/com/fiap/techchallenge/application/domain/service/order/UpdateOrderService.java
@@ -6,16 +6,19 @@ import com.fiap.techchallenge.application.port.in.order.UpdateOrderCommand;
 import com.fiap.techchallenge.application.port.in.order.UpdateOrderUseCase;
 import com.fiap.techchallenge.application.port.out.order.UpdateOrderPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class UpdateOrderService implements UpdateOrderUseCase {
 
     private final UpdateOrderPort updatePort;
 
     @Override
     public Order updateOrder(UpdateOrderCommand command) throws ResourceNotFoundException {
+        log.info("Updating order {} to status {}", command.entityId(), command.status());
         return updatePort.updateOrder(command.entityId(), command.status());
     }
 }
--- src/main/java/com/fiap/techchallenge/application/domain/service/product_category/CreateProductCategoryService.java
+++ src/main/java/com/fiap/techchallenge/application/domain/service/product_category/CreateProductCategoryService.java
@@ -4,16 +4,20 @@ import com.fiap.techchallenge.application.domain.model.ProductCategory;
 import com.fiap.techchallenge.application.port.in.product_category.CreateProductCategoryUseCase;
 import com.fiap.techchallenge.application.port.out.product_category.CreateProductCategoryPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class CreateProductCategoryService implements CreateProductCategoryUseCase {
 
     private final CreateProductCategoryPort registerPort;
 
     @Override
     public ProductCategory createProductCategory(ProductCategory domainEntity) {
+        log.info("Creating ProductCategory {} with description {}",
+            domainEntity.getName(), domainEntity.getDescription());
         return registerPort.createProductCategory(domainEntity);
     }
 }
